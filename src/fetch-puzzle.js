496746c8cf24c5568cf1ab117f8e4588cc04743f
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-11-15 16:55:53 +0800

    Update fetch-puzzle.js

--- src/fetch-puzzle.js
+++ src/fetch-puzzle.js
@@ -12,21 +12,21 @@ export const fetchPuzzle = async ({
     setStatus("");
 
     try {
-        // const res = await fetch(SUDOKU_API);
-        // const data = await res.json();
+        const res = await fetch(SUDOKU_API);
+        const data = await res.json();
 
-        // const grid = data.newboard.grids[0];
-        // const puzzle = grid.value.map((row) => 
-        //     row.map(cell => cell === 0 ? null : cell)
-        // );
-        // const solution = grid.solution.map((row) => 
-        //     row.map(cell => cell === 0 ? null : cell)
-        // );
+        const grid = data.newboard.grids[0];
+        const puzzle = grid.value.map((row) => 
+            row.map(cell => cell === 0 ? null : cell)
+        );
+        const solution = grid.solution.map((row) => 
+            row.map(cell => cell === 0 ? null : cell)
+        );
 
-        // setPuzzle(puzzle);
-        // setSolution(solution);
-        // setBoard(puzzle.map((row) => [...row]));
-        // setSelected(null);
+        setPuzzle(puzzle);
+        setSolution(solution);
+        setBoard(puzzle.map((row) => [...row]));
+        setSelected(null);
     } catch (e) {
         setError("Failed to fetch puzzle.", e);
     }
